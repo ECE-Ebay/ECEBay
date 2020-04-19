51408df146cdec513716d5ce4296add166d7ba9b
Lahkyk <47594086+Lahkyk@users.noreply.github.com>
2020-04-19 09:42:14 +0200

    Add files via upload

--- inscription1.php
+++ inscription1.php
@@ -34,7 +34,7 @@ if(isset($_POST['forminscription']))
                     $mailexist=$reqmail->rowCount();
                     if($mailexist==0)
                     {
-                        if($mdp==$mdp2)
+                        if($mdp==$mdp2) //verifie que les deux mdp correspondent
                         {
                            $insertmbr= $bdd->prepare("INSERT INTO membres(pseudo, mail, motdepasse)VALUES(?, ?, ?)");
                             //prepare une requète qui va envoyé les données remplies par l'utilisateur dans la bdd
@@ -91,9 +91,21 @@ if(isset($_POST['forminscription']))
     <title>login page</title>
     <meta charset="utf-8">
 </head>
-<body>
+<body style="background-color: #111111;">
 
-    <div align="center">
+    <nav class="navbar navbar-expand-md">    
+        <img src="Logo ECEBay.png" alt="AllFruits Logo" style="width:100px;height:100px;">
+        <button class="navbar-toggler navbar-dark" type="button" datatoggle="collapse" data-target="#main-navigation">     
+            <span class="navbar-toggler-icon"></span>    
+        </button>    
+        <div class="collapse navbar-collapse" id="main-navigation">     
+            <ul class="navbar-nav">      
+
+            </ul>    
+        </div>  
+    </nav>
+
+    <div align="center"  style="color: #c1c1c1">
         <h2>Inscription : </h2>
 
         <br /><br />
@@ -136,8 +148,8 @@ if(isset($_POST['forminscription']))
             <tr>
                 <td></td>
                 <td>    
-                    <input type="submit" name='forminscription' value="Suite"/>
-                    <p>Déjà inscrit ?<a href=connexion.php>Me connecter</a> </p>
+                    <input style="background-color: #6f6f6f; color: #ffffff" type="submit" name='forminscription' value="Suite"/>
+                    <p style="color: #c1c1c1"><br>Déjà inscrit ?<a href=connexion.php>Me connecter</a> </p>
                 </td>
             </tr>
         </form>
@@ -149,5 +161,28 @@ if(isset($_POST['forminscription']))
         ?>
 
     </div>
+
+    <footer class="page-footer">    
+        <div class="container">     
+            <div class="row">      
+                <div class="col-lg-8 col-md-8 col-sm-12">       
+                    <h6 class="text-uppercase font-weight-bold">Information additionnelle</h6>       
+                    <p>        Nous sommes un collectif d'étudiants ayant pour but de redonner envie au gens de cultiver       </p>       
+                    <p>        Notre objectif est de transmettre ces connaissances et de permettre aux personnes animées par cette passion de se rencontrer       </p>      
+                </div>     
+
+                <div class="col-lg-4 col-md-4 col-sm-12">       
+                    <h6 class="text-uppercase font-weightbold">Contact</h6>       
+                    <p> 40, avenue des Ternes, 75017 Paris, France <br>        
+                        info@ECEbay.ece.fr <br>        
+                        +33 01 03 05 07 09 <br>        
+                        +33 06 05 04 03 02
+                    </p>      
+                </div>    
+            </div>    
+
+            <div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d'auteur: ECEBay@ece.fr</div>  
+        </div>
+    </footer> 
 </body>
 </html>
