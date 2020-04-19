0db6234f81aeffa3a399c249d835ab7a8ae20952
Sahkto <58793808+Sahkto@users.noreply.github.com>
2020-04-19 02:40:22 +0200

    Add files via upload

--- page principal.php
+++ page principal.php
@@ -9,6 +9,7 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="style.css">  
+	<script type="text/javascript" src="js/app.js"></script>
 	<script type="text/javascript">   
 	$(document).ready(function(){ $('.header').height($(window).height()); $('.scroll-fade').height($(window).height());   });
 	</script> 
@@ -33,6 +34,7 @@
             });
         
         </script>
+        <style> body {   padding: 0px;   margin: 0px;   background: #000; } </style>
 </head> 
 <body>  
 	<nav class="navbar navbar-expand-md">    
