7a0eac94b8cb0c1b59c8a2c7bb9aa794c2fc26a2
LouisFieux <47658694+LouisFieux@users.noreply.github.com>
2020-04-20 21:04:40 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+ <footer class="page-footer">    
+ 	<div class="container">     
+ 		<div class="row">      
+ 			<div class="col-lg-8 col-md-8 col-sm-12">       
+ 				<h6 class="text-uppercase font-weight-bold">Informations additionnelles</h6>       
+ 				<p>        Nous sommes un collectif de passionnés ayant pour but de mettre en contact des particuliers pour leur vente et achat de véhicules       </p>    
+ 			</div>      
+ 			<div class="col-lg-4 col-md-4 col-sm-12">       
+ 				<h6 class="text-uppercase font-weightbold">Contact</h6>       
+ 				<p> 40, avenue des Ternes, 75017 Paris, France <br>        
+ 					info@ECEbay.ece.fr <br>        
+ 					+33 01 03 05 07 09 <br>        
+ 					+33 06 05 04 03 02</p>      
+ 			</div>    
+ 		</div>    
+ 		<div class="footer-copyright text-center">&copy; 2020 Copyright | Droit d'auteur: ECEBay@ece.fr</div>  
+ 	</div>
+ </footer> 
+
